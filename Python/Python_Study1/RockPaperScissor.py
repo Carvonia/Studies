a7bb753b52316f9e262d0fd38e4b096d155ee1e4
Carvonia <mathew.170105@gmail.com>
2023-04-07 01:38:00 -0300

    Testing signing again

--- Python/Python_Study1/RockPaperScissor.py
+++ Python/Python_Study1/RockPaperScissor.py
@@ -1,19 +1,24 @@
 import random
 import os
 
-warriors = {1:'Rock', 2:'Paper', 3:'Scissors'}
+warriors = ('Rock', 'Paper', 'Scissors')
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
     
 def main(p_choice):
-    cpu_choice = warriors[random.randint(1, 3)]
+    cls()
+    cpu_choice = random.choice(warriors)
+    
+    print('Your Warrior: '+p_choice)
+    print('Cpu`s Warrior: '+cpu_choice)
+    
     if cpu_choice == p_choice:
-        print(f'\nIt`s a tie! You both choose {cpu_choice}')
+        print(f'\nIt`s a tie!')
     elif (cpu_choice == 'Rock' and p_choice == 'Paper') or (cpu_choice == 'Paper' and p_choice == 'Scissors') or (cpu_choice == 'Scissors' and p_choice == 'Rock'):
-        print(f'\nYou won! The cpu picked {cpu_choice}!')
+        print(f'\nYou won! :)')
     elif (cpu_choice == 'Rock' and p_choice == 'Scissors') or (cpu_choice == 'Paper' and p_choice == 'Rock') or (cpu_choice == 'Scissors' and p_choice == 'Paper'):
-        print(f'\nYou lose! The cpu picked {cpu_choice}!')
+        print(f'\nYou lose! :(')
     else:
         cls()
         print('You picked an invalid number, try again.\n')
@@ -24,8 +29,8 @@ def main(p_choice):
 
 while True:
     try:
-        loop = main(warriors[int(input('Pick a warrior: \n1. Rock (Defense ++) (Bludgeon +++) \n2. Paper (Sharpness +++) (Swiftness +) \n3. Scissors (Cutting +++) (Resistance -)\n'))])
+        loop = main(str(input('Pick a warrior: \nRock (Defense ++) (Bludgeon +++) \nPaper (Sharpness +++) (Swiftness +) \nScissors (Cutting +++) (Resistance -)\n')))
         if loop == 'n' or loop == 'N': break
     except ValueError:
         cls()
-        print('Use only full numbers please!\n')
+        print('Write the warrior`s name as shown please!\n')
