1e96a117a5340a40f01d4403bec77c6e1179f678
Carvonia <mathew.170105@gmail.com>
2023-04-07 13:36:18 -0300

    Testing ssh again

--- Python/Python_Study1/ClassTests.py
+++ Python/Python_Study1/ClassTests.py
@@ -7,17 +7,6 @@ class Multiply():
         x = Val1 * Val2
         return x
 
-#text_model = 'The {} is {}!\n'
-#def form(*args, **kwargs):
-#    sum = 0
-#    for key,value in kwargs.items():
-#        print(text_model.format(key,value))
-#    for i in args:
-#        sum += i
-#    return sum
-        
-#print(form(1, 2, 3, 4, Cat='Cute', Dog='Happy', Parrot='Singing'))
-
 Employees = []
 
 def format_currency(i):
