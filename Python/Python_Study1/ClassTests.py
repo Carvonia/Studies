897fc543cb95946f43e147cb284a07b654e2feac
Carvonia <mathew.170105@gmail.com>
2023-04-13 05:13:40 -0300

    Improvements overall Made the rock, paper, scissors game more Object Oriented. Studied some more function and classes and added to the code!

--- Python/Python_Study1/ClassTests.py
+++ Python/Python_Study1/ClassTests.py
@@ -1,9 +1,10 @@
 import locale
-locale.setlocale( locale.LC_ALL, '' )
 
-class Multiply():
+locale.setlocale(locale.LC_ALL, "")
+
+
+class Multiply:
     @classmethod
     def operate(cls, Val1, Val2):
         x = Val1 * Val2
         return x
-
--- Python/Python_Study1/RockPaperScissor.py
+++ Python/Python_Study1/RockPaperScissor.py
@@ -1,37 +1,85 @@
 import random
 import os
 
-warriors = ('Rock', 'Paper', 'Scissors')
+outcomes = {
+    ("Rock", "Paper"): "Your troops immobilized the Rocks, gaining you an advantage.",
+    ("Paper", "Scissors"): "Your troops sliced thy enemies into pieces, no one shall stand!",
+    ("Scissors", "Rock"): "The blunt force of your Rocks cracked the enemy`s defences!",
+    ("Rock", "Scissors"): "Sun foresaw your strategies, your troops are no more...",
+    ("Paper", "Rock"): "The enemy sent his forces to immobilize your troops, he is closing on you!",
+    ("Scissors", "Paper"): "Your troops were nothing facing the enemy`s blades."
+}
+warriors = ["Rock", "Scissors", "Paper"]
+score = 0
+
 
 def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
-def main(p_choice):
-    cls()
-    p_choice = p_choice.capitalize()
-    cpu_choice = random.choice(warriors)
-    
-    print('Your Warrior: '+p_choice)
-    print('Cpu`s Warrior: '+cpu_choice)
-    
-    if cpu_choice == p_choice:
-        print(f'\nIt`s a tie!')
-    elif (cpu_choice == 'Rock' and p_choice == 'Paper') or (cpu_choice == 'Paper' and p_choice == 'Scissors') or (cpu_choice == 'Scissors' and p_choice == 'Rock'):
-        print(f'\nYou won! :)')
-    elif (cpu_choice == 'Rock' and p_choice == 'Scissors') or (cpu_choice == 'Paper' and p_choice == 'Rock') or (cpu_choice == 'Scissors' and p_choice == 'Paper'):
-        print(f'\nYou lose! :(')
-    else:
-        cls()
-        print('You picked an invalid warrior, try again.\n')
-        return 0
-    loop = str(input('Do you want to play again? (y/n): '))
-    cls()
-    return loop
+    os.system("cls" if os.name == "nt" else "clear")
 
+
+def pick_warrior():
+    while True:
+        warrior_input = str(
+            input(
+                f"Pick a warrior: \nRock (Defense ++) (Bludgeon +++) \nPaper (Sharpness +++) (Swiftness +) \nScissors (Cutting +++) (Resistance -)\n"
+            )
+        )
+        warrior_input = warrior_input.capitalize()
+        if warrior_input in warriors:
+            cls()
+            return warrior_input
+        else:
+            cls()
+            print(f"You picked an invalid warrior, try again.\n")
+
+
+def score_update_print(outcome, score):
+    if "gain" in outcome or "sliced" in outcome or "cracked" in outcome:
+        score += 100
+        print(f"\n{outcome}")
+        print(f"\nYou gained 100 points!")
+        print(f"Your current score is {score}!")
+    else:
+        score -= 50
+        print(f"\n{outcome}")
+        print(f"\nYou lost 50 points!")
+        print(f"Your current score is {score}!")
+    return score
+
+def main(p_choice, score):
+    cpu_choice = random.choice(warriors)
+
+    print(f"Your Warrior: {p_choice}")
+    print(f"Cpu`s Warrior: {cpu_choice}")
+
+    if cpu_choice == p_choice:
+        print(f"\nYou both sent the same type of troops. The battle results in a Tie!")
+        print(f"Your score remains {score}.")
+    else:
+        outcome = outcomes.get((cpu_choice, p_choice))
+        score = score_update_print(outcome, score)
+
+    return score
+
+
+cls()
+print(
+    f"You dear friend, are a general, a commander preparing to send soldiers to the battlefield."
+)
+print(f"Your enemy is the great Sun Tzu, he will have no mercy on your troops.")
+print(f"Choose your soldiers carefully, and reach 300 points to win the war!\n")
 while True:
-    try:
-        loop = main(str(input('Pick a warrior: \nRock (Defense ++) (Bludgeon +++) \nPaper (Sharpness +++) (Swiftness +) \nScissors (Cutting +++) (Resistance -)\n')))
-        if loop == 'n' or loop == 'N': break
-    except ValueError:
-        cls()
-        print('Write the warrior`s name as shown please!\n')
+    user_input = pick_warrior()
+    score = main(user_input, score)
+    if score >= 300:
+        print(f"\nYou won the war, now going foward to conquering the whole world!")
+        break
+    elif score <= -150:
+        print(f"\nYou lost the war, the enemy now lavishes uppon your land...")
+        break
+    is_loop = str(input(f"\nDo you want to play again? (y/n): ")).lower()
+    cls()
+    if is_loop == "y" or is_loop == "":
+        print(f"Your current score is {score}. ", end="")
+        continue
+    break
